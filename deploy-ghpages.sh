a4b4c216527a606e46ee678e221146fdd9eb7997
Callum White <callumw1991@gmail.com>
2014-12-08 08:15:53 +1000

    commit

--- deploy-ghpages.sh
+++ deploy-ghpages.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
-rm -rf out || exit 0;
-mkdir out;
-cd out;
-git init;
-git config user.name "cwhitey";
-git config user.email "callumw1991@gmail.com";
-git add .;
-git commit -m "Deployed to GitHub Pages";
-git push --force... #UNFINISHED 
+
+echo "test";
+
+#rm -rf out || exit 0;
+#mkdir out;
+#cd out;
+#git init;
+#git config user.name "cwhitey";
+#git config user.email "callumw1991@gmail.com";
+#git add .;
+#git commit -m "Deployed to GitHub Pages";
+#git push --force... #UNFINISHED
