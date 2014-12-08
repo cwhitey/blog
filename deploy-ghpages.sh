b8655d17ec2d3e303ff09ee3e0761c539fd6db43
callum white <callumw1991@gmail.com>
2014-12-08 17:09:48 +1100

    Starting to get set up using Travis CI so I don't have to manually use buster.py every time I'm making a post

---
+++ deploy-ghpages.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+rm -rf out || exit 0;
+mkdir out;
+cd out;
+git init;
+git config user.name "cwhitey";
+git config user.email "callumw1991@gmail.com";
+git add .;
+git commit -m "Deployed to GitHub Pages";
+git push --force... #UNFINISHED 
