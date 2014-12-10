d18a097141e8da7fdec454ff95d3b3d89f29a8b9
Callum White <callumw1991@gmail.com>
2014-12-10 22:29:28 +1100

    Changed deploy-ghpages.sh

--- deploy-ghpages.sh
+++ deploy-ghpages.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 python buster.py setup --gh-repo="http://cwhitey.github.io";
-npm start > /dev/null 2>&1 &;
+npm start > /dev/null 2>&1 &
 python buster.py generate --domain="localhost:2368";
 cd static;
 git init;
