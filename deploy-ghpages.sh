a45a17f5d13d2c42c10a8ce2bf37d96411456344
Callum White <callumw1991@gmail.com>
2014-12-10 22:23:18 +1100

    Changed deploy-ghpages.sh

--- deploy-ghpages.sh
+++ deploy-ghpages.sh
@@ -1,14 +1,11 @@
 #!/bin/bash
-mkdir out;
-cd out;
+python buster.py setup --gh-repo="http://cwhitey.github.io";
+npm start > /dev/null 2>&1 &;
+python buster.py generate --domain="localhost:2368";
+cd static;
 git init;
-git config user.name "Callum White";
-git config user.email "callumw1991@gmail.com";
-python buster.py setup --gh-repo="http://cwhitey.github.io"
-npm start > /dev/null 2>&1 &; #don't worry about killing this?
-python buster.py generate --domain="localhost:2368"
-cd static/;
-
+git config user.name $GIT_NAME;
+git config user.email $GIT_EMAIL;
 git add --all;
 git commit -m "Deployed GitHub Pages by Travis (build $TRAVIS_BUILD_NUMBER)";
-git push --force
+git push --force "http://cwhitey.github.io";
