3bb5b9923c7d45cfe7aacb0c98696c17e3dd657f
callum white <callumw1991@gmail.com>
2014-12-10 17:10:30 +1100

    commit

--- deploy-ghpages.sh
+++ deploy-ghpages.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
+mkdir out;
+cd out;
+git init;
+git config user.name "Callum White";
+git config user.email "callumw1991@gmail.com";
+python buster.py setup --gh-repo="http://cwhitey.github.io"
+npm start > /dev/null 2>&1 &; #don't worry about killing this?
+python buster.py generate --domain="localhost:2368"
+cd static/;
 
-echo "test";
-
-#rm -rf out || exit 0;
-#mkdir out;
-#cd out;
-#git init;
-#git config user.name "cwhitey";
-#git config user.email "callumw1991@gmail.com";
-#git add .;
-#git commit -m "Deployed to GitHub Pages";
-#git push --force... #UNFINISHED
+git add --all;
+git commit -m "Deployed GitHub Pages by Travis (build $TRAVIS_BUILD_NUMBER)";
+git push --force
