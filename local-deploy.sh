6bc17cf92e67d0efb786a9db4732dabd19d52b88
callum white <callumw1991@gmail.com>
2014-12-11 13:58:56 +1100

    Updated some css, and the local-deploy script (it works)

--- local-deploy.sh
+++ local-deploy.sh
@@ -3,12 +3,15 @@
 #Assumes Buster is installed with PIP, we're in the /blog/ repo
 #and there is a ../cwhitey.github.io/ repo checked out
 
-rm -rf ../static
-buster setup --gh-repo="http://cwhitey.github.io";
 cd ../;
+rm -rf static/;
+buster setup --gh-repo="http://cwhitey.github.io";
+echo "GENERATING STATIC PAGE...";
 buster generate --domain="localhost:2368";
 cp -R static/* cwhitey.github.io/;
 cd cwhitey.github.io/;
 git add --all;
 git commit -m "Updated GitHub Pages";
+echo "";
+echo "FORCE PUSHING TO GITHUB PAGES...";
 git push --force;
